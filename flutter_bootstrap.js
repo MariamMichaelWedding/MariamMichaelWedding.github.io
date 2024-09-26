ec0fac4f75c2b499e1f6ffc913e857a6dad4f4ec
MariamMichaelWedding <amirmamdouhnassif@gmail.com>
2024-09-26 12:54:33 +0300

    Changed The ending

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "486631875"
+    serviceWorkerVersion: "2115450259"
   }
 });
